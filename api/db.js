cf7b6078fae4637caa2931fa97519f91dfa2d5c1
Machabelele Manaka <120823140+machabelele@users.noreply.github.com>
2023-08-20 12:13:13 +0200

    commit 2

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "a16072@@2B",
+  password: process.env.DB_KEY,
   database: "blog",
 });
